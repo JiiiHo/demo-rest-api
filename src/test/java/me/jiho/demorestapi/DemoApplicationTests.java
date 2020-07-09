9fd1d9d08c85c0b148276d859f504894e5eff253
jiiiho <tlswlgh120@naver.com>
2020-07-09 20:31:10 +0900

    init

---
+++ src/test/java/me/jiho/demorestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jiho.demorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
